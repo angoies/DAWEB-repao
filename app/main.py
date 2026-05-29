706e936f063c0d887a183f7040a3d32be58b9178
jgomez <jgomez@romero.com>
2026-05-29 13:29:50 +0200

    modifico main para probar actio v1.0

--- app/main.py
+++ app/main.py
@@ -11,5 +11,5 @@ app.include_router(items.router)
 # Código que se añade
 @app.get("/status")
 def version():
-    return {"status": "Apellidos, Nombre - v.X.Y"}
+    return {"status": "Ango, Nombre - v.1.0"}
 
